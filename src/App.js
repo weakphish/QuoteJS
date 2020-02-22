d0cfa17110122698cb37c9b5e81b98d1c7d4769f
John Allison <john123allison@gmail.com>
2020-02-21 19:37:39 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function fetchQuote() {
+    return fetch('http://quotes.stormconsultancy.co.uk/random.json') // fetch a response from the api
+        .then((response) => { 
+            let json = response.json(); // then assign the JSON'd response to a var
+            return json; // return that bad boy
+    });
+}
+
+/* component for the quote */
+function Quote(props) {
+    return (
+        <div>
+            <h1>{props.quoteAuthor}</h1>
+            <p>{props.quote}</p>
+        </div>
+    );
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /* assign placeholder values for the text */
+    let quoteAuthor = "";
+    let quote = "";
+
+    /* async fetch the quotes and reassign the variables to them once processed */
+    fetchQuote().then((response) => {
+        let json = response;
+        console.log(json); // debug
+        quoteAuthor = json.author;
+        quote = json.quote;
+    });
+
+    /* return a JSX formatted component stuff */
+    return (
+        <div className = "Main">
+            <Quote author = {quoteAuthor} quote = {quote} />
+        </div>
+    );
 }
 
 export default App;
