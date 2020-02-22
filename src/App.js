de7b1a827439f370b7ecd913baf55be34d57f28e
John Allison <john123allison@gmail.com>
2020-02-21 19:56:41 -0500

    things render, I guess?

--- src/App.js
+++ src/App.js
@@ -10,32 +10,47 @@ function fetchQuote() {
 }
 
 /* component for the quote */
-function Quote(props) {
-    return (
+class Quote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {author: "", quote: ""}
+    }
+    render() {
+        return (
         <div>
-            <h1>{props.quoteAuthor}</h1>
-            <p>{props.quote}</p>
+            <h1>{this.state.quoteAuthor}</h1>
+            <p>{this.state.quote}</p>
         </div>
-    );
+        );
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          20000
+        );
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    /* async fetch the quotes and reassign the variables to them once processed */
+    async tick() {
+        let response = await fetchQuote();
+        console.log(response);
+        this.setState({
+            quoteAuthor: response.author,
+            quote: response.quote
+        });
+    }
+    
 }
 
 function App() {
-    /* assign placeholder values for the text */
-    let quoteAuthor = "";
-    let quote = "";
 
-    /* async fetch the quotes and reassign the variables to them once processed */
-    fetchQuote().then((response) => {
-        let json = response;
-        console.log(json); // debug
-        quoteAuthor = json.author;
-        quote = json.quote;
-    });
 
     /* return a JSX formatted component stuff */
     return (
         <div className = "Main">
-            <Quote author = {quoteAuthor} quote = {quote} />
+            <Quote />
         </div>
     );
 }
