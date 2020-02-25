77887f17062c01c014e4bf294774b55a17ff0355
John Allison <john123allison@gmail.com>
2020-02-24 21:33:07 -0500

    buttons

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -7,7 +7,10 @@ export default class Quote extends React.Component {
     /* placeholder */
     constructor(props) {
         super(props);
-        this.state = {quoteAuthor: "Rick Osborne", quote: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live."}
+        this.state = {
+            quoteAuthor: "Rick Osborne", 
+            quote: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live."
+        }
     }
     /* actually render things */
     render() {
@@ -15,27 +18,20 @@ export default class Quote extends React.Component {
                 <div className="main">
                     <h1>{this.state.quoteAuthor}</h1>
                     <p>{this.state.quote}</p>
+                    <div className="button">
+                        <button id="button" onClick={this.update}>New quote</button>
+                    </div>
                 </div>
         );
     }
-    /* start a timer */
-    componentDidMount() {
-        this.timerID = setInterval(
-          () => this.tick(),
-          20000
-        );
-    }
-    /* idk what this does but facebook told me to do it */
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
+    
     /* async fetch the quotes and reassign the variables to them once processed */
-    async tick() {
+    update = async() => {
         let response = await fetchQuote();
         console.log(response);
         this.setState({
             quoteAuthor: response.author,
             quote: response.quote
         });
-    }   
+    };   
 }
